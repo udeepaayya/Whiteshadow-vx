e5105c963782ead354b1897de4d4c0a873d1e0d8
cnw-db <newbot1952@gmail.com>
2025-08-11 18:36:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "White-MD~OYdHDChD#iDBrYDvGdKZjcwl98GffqnrO3vmFRRfbBPAHtUcY1zs",
+SESSION_ID: process.env.SESSION_ID || "White-MD~qcdmGKpL#O6uRvHHDvaBcEAWqPDSH-XDE4sW5ZFl5dpJeo_9ySak",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
