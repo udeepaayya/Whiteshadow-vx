485c176f44527299655be983a848b008957c23b8
chamod-db <newbot1952@outlook.com>
2025-08-11 17:27:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "White-MD~3Ed2STgY#rSbgADX4o9bSGO-pxsVxCzVw8IrjW6diFdKv-BYgZWc",
+SESSION_ID: process.env.SESSION_ID || "White-MD~OYdHDChD#iDBrYDvGdKZjcwl98GffqnrO3vmFRRfbBPAHtUcY1zs",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
