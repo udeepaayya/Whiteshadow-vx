ed97921a3f015b641d3ba0460e83af3dea074e52
cnw-db <newbot1952@gmail.com>
2025-09-28 01:30:19 +0530

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -1,30 +1,95 @@
-const axios = require('axios');
-const config = require('../config');
-const { cmd } = require('../command');
-
-cmd({
-  on: 'body'
-}, async (conn, mek, m, { from, body }) => {
-  try {
-    const jsonUrl = 'https://raw.githubusercontent.com/chamod-mv/Whiteshadow-data/refs/heads/main/autosticker.json';
-    const res = await axios.get(jsonUrl);
-    const data = res.data;
-
-    for (const keyword in data) {
-      if (body.toLowerCase() === keyword.toLowerCase()) {
-        if (config.AUTO_STICKER === 'true') {
-          await conn.sendMessage(
-            from,
-            {
-              sticker: { url: data[keyword] },
-              package: 'WHITESHADOW-MD'
-            },
-            { quoted: mek }
-          );
+/* Obfuscated by WhiteShadow - https://whatsapp.com/channel/0029Vak4dFAHQbSBzyxlGG13 */
+const _0x4d747c = _0x1c05;
+(function (_0x245f5a, _0x2d0b6b) {
+    const _0x270cb1 = _0x1c05, _0x149343 = _0x245f5a();
+    while (!![]) {
+        try {
+            const _0x44bb39 = -parseInt(_0x270cb1(0x125)) / 0x1 + -parseInt(_0x270cb1(0x131)) / 0x2 * (-parseInt(_0x270cb1(0x12b)) / 0x3) + -parseInt(_0x270cb1(0x138)) / 0x4 + parseInt(_0x270cb1(0x126)) / 0x5 * (-parseInt(_0x270cb1(0x13b)) / 0x6) + -parseInt(_0x270cb1(0x132)) / 0x7 * (parseInt(_0x270cb1(0x127)) / 0x8) + -parseInt(_0x270cb1(0x128)) / 0x9 * (parseInt(_0x270cb1(0x12c)) / 0xa) + parseInt(_0x270cb1(0x12e)) / 0xb * (parseInt(_0x270cb1(0x129)) / 0xc);
+            if (_0x44bb39 === _0x2d0b6b)
+                break;
+            else
+                _0x149343['push'](_0x149343['shift']());
+        } catch (_0x221ee4) {
+            _0x149343['push'](_0x149343['shift']());
         }
-      }
     }
-  } catch (e) {
-    console.error('AutoSticker error:', e);
-  }
+}(_0x53dc, 0xde58c));
+const axios = require(_0x4d747c(0x11f)), config = require(_0x4d747c(0x139)), {cmd} = require(_0x4d747c(0x122)), _0x492732 = {};
+function _0x1c05(_0x1a58ac, _0x88930c) {
+    const _0x53dce5 = _0x53dc();
+    return _0x1c05 = function (_0x1c0557, _0x3b0054) {
+        _0x1c0557 = _0x1c0557 - 0x11b;
+        let _0x443d17 = _0x53dce5[_0x1c0557];
+        return _0x443d17;
+    }, _0x1c05(_0x1a58ac, _0x88930c);
+}
+function _0x53dc() {
+    const _0x24b7a5 = [
+        '2405610kQZzUu',
+        '240KQkJUz',
+        '9DzeOyy',
+        '29703048XSibzn',
+        'NVcVz',
+        '6YNVxuR',
+        '5167770oKOxyz',
+        'TBNyc',
+        '22WzChhp',
+        'MDlhM',
+        'awErz',
+        '525570iaPWTF',
+        '178941tTRrZR',
+        'sendMessage',
+        'true',
+        'data',
+        'toLowerCase',
+        'JctKq',
+        '4441784iJPRza',
+        '../config',
+        'get',
+        '12QxApmV',
+        'AUTO_STICKER',
+        'quoted',
+        'error',
+        'sticker',
+        'axios',
+        'package',
+        'url',
+        '../command',
+        'body',
+        'leZZx',
+        '1208989Oezqbl'
+    ];
+    _0x53dc = function () {
+        return _0x24b7a5;
+    };
+    return _0x53dc();
+}
+_0x492732['on'] = _0x4d747c(0x123), cmd(_0x492732, async (_0x4cc552, _0x48fd45, _0x1594f2, {
+    from: _0x558ab4,
+    body: _0x420ca2
+}) => {
+    const _0x47962e = _0x4d747c, _0x1e5dd6 = {};
+    _0x1e5dd6[_0x47962e(0x137)] = 'https://raw.githubusercontent.com/chamod-mv/Whiteshadow-data/refs/heads/main/autosticker.json', _0x1e5dd6[_0x47962e(0x12d)] = function (_0x1aa2b0, _0x2ea679) {
+        return _0x1aa2b0 === _0x2ea679;
+    }, _0x1e5dd6[_0x47962e(0x12a)] = function (_0x12cc0b, _0x19e14d) {
+        return _0x12cc0b === _0x19e14d;
+    }, _0x1e5dd6[_0x47962e(0x124)] = _0x47962e(0x134), _0x1e5dd6[_0x47962e(0x12f)] = 'WHITESHADOW-MD', _0x1e5dd6['awErz'] = 'AutoSticker\x20error:';
+    const _0x1b90ba = _0x1e5dd6;
+    try {
+        const _0x122c9b = _0x1b90ba[_0x47962e(0x137)], _0x578589 = await axios[_0x47962e(0x13a)](_0x122c9b), _0x4a4b15 = _0x578589[_0x47962e(0x135)];
+        for (const _0x312927 in _0x4a4b15) {
+            if (_0x1b90ba[_0x47962e(0x12d)](_0x420ca2[_0x47962e(0x136)](), _0x312927[_0x47962e(0x136)]())) {
+                if (_0x1b90ba[_0x47962e(0x12a)](config[_0x47962e(0x11b)], _0x1b90ba['leZZx'])) {
+                    const _0x5f03f1 = {};
+                    _0x5f03f1[_0x47962e(0x121)] = _0x4a4b15[_0x312927];
+                    const _0x4bc66e = {};
+                    _0x4bc66e[_0x47962e(0x11e)] = _0x5f03f1, _0x4bc66e[_0x47962e(0x120)] = _0x1b90ba[_0x47962e(0x12f)];
+                    const _0x177ecd = {};
+                    _0x177ecd[_0x47962e(0x11c)] = _0x48fd45, await _0x4cc552[_0x47962e(0x133)](_0x558ab4, _0x4bc66e, _0x177ecd);
+                }
+            }
+        }
+    } catch (_0x581cea) {
+        console[_0x47962e(0x11d)](_0x1b90ba[_0x47962e(0x130)], _0x581cea);
+    }
 });
